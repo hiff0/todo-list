12429a06b9769f51e0e421f69d8040f95deb54a1
Vlad Petrov <hiffchanel@gmail.com>
2023-01-01 20:38:29 +0300

    edited the test for button component

--- my-app/src/components/Button/Button.test.tsx
+++ my-app/src/components/Button/Button.test.tsx
@@ -20,10 +20,28 @@ const title: TitleType = {
 }
 
 describe('Button component', () => {
-    it('renders', () => {
-        render(<Button title={title.add} />);
+    it('renders whit add title and with onClick function', () => {
+        render(<Button title={title.add} onClick={onClick} />);
         expect(screen.getByText(title.add)).toBeInTheDocument();
     })
 
+    it('render with change title and without onClick function', () => {
+        render(<Button title={title.change} />);
+        expect(screen.getByText(title.change)).toBeInTheDocument();
+    })
 
+    it('renders with delete title', () => {
+        render(<Button title={title.delete} onClick={onClick} />);
+        expect(screen.getByText(title.delete)).toBeInTheDocument();
+    })
+
+    it('renders with edit title', () => {
+        render(<Button title={title.edit} onClick={onClick} />);
+        expect(screen.getByText(title.edit)).toBeInTheDocument();
+    })
+
+    it('renders with cancel title', () => {
+        render(<Button title={title.cancel} onClick={onClick} />);
+        expect(screen.getByText(title.cancel)).toBeInTheDocument();
+    })
 })
